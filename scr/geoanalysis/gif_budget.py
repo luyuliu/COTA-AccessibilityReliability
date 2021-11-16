80290b8fcd5c62c4a5f3c179c9181bf864b8b256
Luyu Liu <liuluyu0378@gmail.com>
2021-11-15 19:24:22 -0500

    Generate GIFs

--- scr/geoanalysis/gif_budget.py
+++ scr/geoanalysis/gif_budget.py
@@ -1,27 +1,31 @@
 import glob
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 from datetime import timedelta, date, datetime
 # filepaths
 
-breakpointList = [date(2018, 1, 1), date(2018, 5, 1), date(2018, 9, 1), date(
-    2019, 1, 1), date(2019, 5, 1), date(2019, 9, 1), date(2020, 1, 1)]
-timepointList = [8, 12, 18]
+breakpointList = [date(2019, 9, 1)]
+timepointList = [8]
 budgetList = [i for i in range(5, 121, 5)]
 
 
 for i in breakpointList:
     for j in timepointList: 
-        fp_in = r"D:\Luyu\reliability\serious_reliability\pdfs\REA_project_REA_" + i.strftime("%Y%m%d") + "_" + str(j) + "_*.png"
-        fp_out = r"D:\Luyu\reliability\serious_reliability\gifs\timebudget\REA_" + i.strftime("%Y%m%d") + "_" + str(j) + ".gif"
-
-        name_list = []
+        fp_out = r"D:\Luyu\reliability\serious_reliability\pngs\Sep19Gif.gif"
+        img_list = []
         for k in budgetList:
-            name_list.append(r"D:\Luyu\reliability\serious_reliability\pdfs\REA_project_REA_" + i.strftime("%Y%m%d") + "_" + str(j) + "_" + str(k) + ".png")
-
+            fp_int = r"D:\Luyu\reliability\serious_reliability\pngs\Sep19t_" + str(k) + ".png"
+            fp_in = r"D:\Luyu\reliability\serious_reliability\pngs\Sep19_" + str(k) + ".png"
+            img = Image.open(fp_in) 
+            draw = ImageDraw.Draw(img)
+            font = ImageFont.truetype("D:\\Luyu\\reliability\\serious_reliability\\"+"OpenSans-Regular.ttf", 30)
+            # draw.text((x, y),"Sample Text",(r,g,b))
+            draw.text((0, 0),"Time budget: " + str(k) + " minutes",(0,0,0), font=font)
+            img.save(fp_int)
+            img_list.append(fp_int)
         # a = sorted(glob.glob(fp_in))
         # print(name_list)
-        img, *imgs = [Image.open(f) for f in name_list]
+        img, *imgs = [Image.open(f) for f in img_list]
         img.save(fp=fp_out, format='GIF', append_images=imgs,
-                save_all=True, duration=500, loop=0)
+                save_all=True, duration=600, loop=0)
     #     break
     # break
